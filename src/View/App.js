54241b3eab6c5d15bbd657486ff9cb1dd8481288
YosephAhmed <yosephahmed@aol.com>
2020-06-11 14:07:31 -0400

    Cleaned up un-needed files that were created during create-react-app.

---
+++ src/View/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <React.Fragment><h1>App</h1></React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './View/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
